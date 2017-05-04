7ec88234b91c14b8ecbf0c44c75a7edf039ce56a
bartek <bartek+github@szabat.com>
2017-05-04 09:16:20 +0200

    geotools first experiment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,16 @@
  * Copyright (C) 2017 Bartek.
  */
 
+
+import java.util.ArrayList;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
+
 /**
  * Main class.
  *
@@ -9,5 +19,22 @@
  */
 public class Main {
     public static void main(String[] args) {
+      final GeometryFactory gf = new GeometryFactory();
+
+      final ArrayList<Coordinate> points = new ArrayList<Coordinate>();
+      points.add(new Coordinate(-10, -10));
+      points.add(new Coordinate(-10, 10));
+      points.add(new Coordinate(10, 10));
+      points.add(new Coordinate(10, -10));
+      points.add(new Coordinate(-10, -10));
+      final Polygon polygon = gf.createPolygon(new LinearRing(new CoordinateArraySequence(points
+        .toArray(new Coordinate[points.size()])), gf), null);
+
+      final Coordinate coord = new Coordinate(0, 0);
+      final Point point = gf.createPoint(coord);
+
+      System.out.println(point.within(polygon));
+
+
     }
 }
