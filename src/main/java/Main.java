f17e25db1adf9796a0e86c7be81e22511c459a62
bartek <bartek+windows@szabat.com>
2017-05-04 10:28:33 +0200

    open shape file experiment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@
  */
 
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -12,6 +13,14 @@ import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 
+import java.io.File;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.DefaultMapContext;
+import org.geotools.map.MapContext;
+
 /**
  * Main class.
  *
@@ -19,6 +28,26 @@ import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
  */
 public class Main {
     public static void main(String[] args) {
+
+
+      try {
+        FileDataStore store = FileDataStoreFinder.getDataStore(new File("C:/Users/Bartek/Downloads/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp"));
+
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        String s = featureSource.getBounds().toString();
+
+         // Create a map context and add our shapefile to it
+        MapContext map = new DefaultMapContext();
+        map.setTitle("Quickstart");
+        map.addLayer(featureSource, null);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+
+
+
       final GeometryFactory gf = new GeometryFactory();
 
       final ArrayList<Coordinate> points = new ArrayList<Coordinate>();
