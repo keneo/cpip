01ead22c9c4b4a6c9df3dff2aa772f22d27cb926
bartek <bartek+windows@szabat.com>
2017-05-05 14:02:33 +0200

    figure out tests

---
+++ src/test/java/CountryLocatorQuadTreeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bartek on 05/05/2017.
+ */
+public class CountryLocatorQuadTreeTest {
+  @Test
+  public void getCountryAt() throws Exception {
+  }
+
+}
