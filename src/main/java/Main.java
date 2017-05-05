aa85447b11c0503afc65b70a58fb0f43a719d4a8
bartek <bartek+windows@szabat.com>
2017-05-05 12:47:15 +0200

    nvm there was no bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -14,25 +15,31 @@ public class Main {
 
         MapSource source = new MapSource("C:/Users/Bartek/Downloads/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp");
         CountryLocator locator = new CountryLocator(source);
-        Random rnd = new Random(8);
+        Random rnd = new Random(5);
 
-        for (int i=0; i<1000000; i++) {
-          double x = rnd.nextDouble() * 180-90;
-          double y = rnd.nextDouble() * 360-180;
+        for (int i=0; i<10; i++) {
+          double x = rnd.nextDouble() * 360-180;
+          double y = rnd.nextDouble() * 180-90;
 
           //x=0.0;
           //y=51.0;
 
-          System.out.print("At point: "+x+","+y+": ");
+          log("At point: "+x+","+y+": ");
 
           String country = locator.getCountryAt(x, y);
 
-          System.out.println(country);
+          log(country);
         }
 
       } catch (IOException e) {
         e.printStackTrace();
       }
 
+
+
     }
+
+  private static void log(String line) {
+    System.out.println(new Date().toString() + " " +line);
+  }
 }
